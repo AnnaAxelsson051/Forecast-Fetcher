c73e51816cb1b900f97f047f46bfab61dbba06f2
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-22 23:10:27 +0200

    Add empty files

---
+++ src/main/java/com/example/weatherwebservice/WeatherWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weatherwebservice/WeatherWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
